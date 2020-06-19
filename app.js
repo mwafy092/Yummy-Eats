015dd05a2ef3544f2c8e15fef6e794e00e489047
mwafy092 <mwafy092@gmail.com>
2020-06-19 13:27:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('#btn');
+let overlay = document.querySelector('#overlay');
+
+btn.addEventListener('click', () => {
+    // add grid to display and animations
+    overlay.style.display = "grid";
+    overlay.classList.add('overlay-animation');
+
+    // set display to none after 3 seconds 
+    setTimeout(function () {
+        overlay.style.display = "none";
+    }, 2000);
+});
